00b91ac742dcc1a7f530e48ecf3cdec1bfe7f52b
amiriog7-cyber <amiriog7@gmail.com>
2026-06-02 04:17:13 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        webView.loadUrl("file:///android_asset/calculator.html");
+        webView.loadUrl("https://ahwaz.gt.tc/calculator.html");
     }
 
     @Override
