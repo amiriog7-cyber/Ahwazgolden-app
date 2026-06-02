26b080b66f03c5d764ad39666e6c393f7cd9d6e5
amiriog7-cyber <amiriog7@gmail.com>
2026-06-02 04:24:26 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        webView.loadUrl("https://ahwaz.gt.tc/calculator.html");
+        webView.loadUrl("https://ahwazgolden.ir/calculator.html");
     }
 
     @Override
